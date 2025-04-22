7f345de01bd39aa02051e42717f461b79a98693f
Eng-Mostafaibrahem <eng.mostafaibrahem99@gmail.com>
2025-04-22 03:10:17 +0200

    try to solve connection mongo atlas

--- DB/connectiondb.js
+++ DB/connectiondb.js
@@ -1,18 +1,30 @@
 import mongoose from "mongoose";
+// db.js
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 export const connection_db = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.CONNECTION_DB_URI, {
+      dbName: "NoteApp",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000,
+    }).then(mongoose => mongoose);
+  }
+
   try {
-    await mongoose.connect(
-      process.env.CONNECCTION_DB_URI,
-      { dbName: "NoteApp" },
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        serverSelectionTimeoutMS: 10000,
-      }
-    );
+    cached.conn = await cached.promise;
     console.log("Database connected");
+    return cached.conn;
   } catch (error) {
-    console.log("connection failed", error);
+    console.log("Connection failed", error);
+    throw error;
   }
 };
