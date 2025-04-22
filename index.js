6b6c42249158bdfa50cd4fa3a3e2748f78d7db7b
Eng-Mostafaibrahem <eng.mostafaibrahem99@gmail.com>
2025-04-22 02:01:16 +0200

    test connection with cluster

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import noteRouter from "./src/note/note.routes.js";
 import { globalResponse } from "./src/Middlewares/error-handde.middleware.js";
 import { config } from "dotenv";
 import path from "path";
+import NoteModel from "./DB/models/note.model.js";
 
 config();
 
@@ -14,6 +15,14 @@ app.use(express.json());
 app.use("/user", userRouter);
 app.use("/note", noteRouter);
 app.use("", (req, res) => res.json({ message: "hello" }));
+app.get("/test-db", async (req, res) => {
+  try {
+    const notes = await NoteModel.find();
+    res.json({ status: "success", notes });
+  } catch (err) {
+    res.status(500).json({ status: "fail", error: err.message });
+  }
+});
 // app.use("/src/uploads", express.static(path.resolve("src/uploads/")))
 app.use(globalResponse);
 
